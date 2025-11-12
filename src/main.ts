e749ea566b6c94408beb7eb900c46425cd696094
thuynh26 <thuynh26@ucsc.edu>
2025-11-12 00:35:55 +0000

    implemented grid index and lat/lng to cell

--- src/main.ts
+++ src/main.ts
@@ -11,19 +11,19 @@ import "./_leafletWorkaround.ts";
 // Import our luck/RNG function -- unused for now
 // import luck from "./_luck.ts"
 
-// =============== GAME PARAMETERS =============== //
+// ============================== GAME PARAMETERS ============================== //
 // Map center is set to classroom
 const MAP_CENTER = leaflet.latLng(36.997936938057016, -122.05703507501151);
 const GAMEPLAY_ZOOM_LVL = 20;
-// const TILE_DEG = 1e-4;
+const TILE_DEGREES = 1e-4;
 
-// =============== UI ELEMENTS =============== //
+// ============================== UI ELEMENTS ============================== //
 
 const mapDiv = document.createElement("div");
 mapDiv.id = "map";
 document.body.append(mapDiv);
 
-// =============== LEAFLET MAP =============== //
+// ============================== LEAFLET MAP ============================== //
 const map = leaflet.map(mapDiv, {
   center: MAP_CENTER,
   zoom: GAMEPLAY_ZOOM_LVL,
@@ -38,14 +38,25 @@ leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // map marker to represent the player
 const playerMarker = leaflet.marker(MAP_CENTER);
-playerMarker.bindTooltip("That's you!");
-playerMarker.addTo(map);
+playerMarker.addTo(map).bindTooltip("That's you!");
 
-// add rectangle to map (represents token cell)
-// const origin = MAP_CENTER;
-const bounds = leaflet.latLngBounds([
-  [36.997936938057016, -122.05703507501151],
-  [36.998036938057014, -122.0569350750115],
-]);
-const rect = leaflet.rectangle(bounds);
-rect.addTo(map);
+// ============================== MAP GRID ============================== //
+function cellBounds(i: number, j: number) {
+  // SW corner (lower-left)
+  const lat0 = MAP_CENTER.lat + i * TILE_DEGREES;
+  const lng0 = MAP_CENTER.lng + j * TILE_DEGREES;
+
+  // NE corner (upper-right)
+  const lat1 = lat0 + TILE_DEGREES;
+  const lng1 = lng0 + TILE_DEGREES;
+  return leaflet.latLngBounds([lat0, lng0], [lat1, lng1]);
+}
+
+const rect1 = leaflet.rectangle(cellBounds(0, 0));
+rect1.addTo(map);
+const rect2 = leaflet.rectangle(cellBounds(0, 1));
+rect2.addTo(map);
+const rect3 = leaflet.rectangle(cellBounds(1, 0));
+rect3.addTo(map);
+const rect4 = leaflet.rectangle(cellBounds(1, 1));
+rect4.addTo(map);
