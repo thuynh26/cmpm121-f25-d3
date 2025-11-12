7b411ebb7998d1553f68b104c240ee7d31972c1c
thuynh26 <thuynh26@ucsc.edu>
2025-11-12 02:50:10 +0000

    added token icon

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,30 @@ import "./_leafletWorkaround.ts";
 // Import our luck/RNG function
 import luck from "./_luck.ts";
 
+import coin1Url from "./coin1.png";
+import coin2Url from "./coin2.png";
+
 // ============================== GAME PARAMETERS ============================== //
 // Map center is set to classroom
 const MAP_CENTER = leaflet.latLng(36.997936938057016, -122.05703507501151);
 const GAMEPLAY_ZOOM_LVL = 19;
 const TILE_DEGREES = 1e-4;
-const TOKEN_SPAWN_PROB = 0.1;
+const TOKEN_SPAWN_PROB = 0.10;
 
 // temp for D3.a use (so that map covers viewable screen)
-const GRID_SIZE = 18;
+const GRID_SIZE = 28;
+
+const tokenIcon1 = leaflet.icon({
+  iconUrl: coin1Url,
+  iconSize: [30, 30],
+  iconAnchor: [15, 15],
+});
+
+const tokenIcon2 = leaflet.icon({
+  iconUrl: coin2Url,
+  iconSize: [30, 30],
+  iconAnchor: [15, 15],
+});
 
 // ============================== UI ELEMENTS ============================== //
 
@@ -49,18 +64,21 @@ const playerMarker = leaflet.marker(MAP_CENTER);
 playerMarker.addTo(map).bindTooltip("That's you!");
 
 // ============================== TOKEN SYSTEM ============================== //
-function spawnToken(i: number, j: number): number | null {
+function spawnToken(i: number, j: number) {
   const spawnRoll = luck([i, j].toString());
-  if (spawnRoll >= TOKEN_SPAWN_PROB) return null;
 
-  const value = spawnRoll ? (Math.random() < 0.5 ? 0 : 1) : null;
+  const value = spawnRoll ? (Math.random() < 0.5 ? 1 : 2) : 0;
+
+  addTokenLabel(i, j, value);
   return value;
 }
 
 function addTokenLabel(i: number, j: number, value: number): leaflet.Marker {
   const center = cellBounds(i, j).getCenter();
+  const icon = value === 1 ? tokenIcon1 : value === 2 ? tokenIcon2 : tokenIcon1;
+
   return leaflet.marker(center, {
-    opacity: 0.5,
+    icon: icon,
   }).addTo(map).bindTooltip(`${value}`);
 }
 
@@ -85,10 +103,7 @@ for (let i = -GRID_SIZE / 2; i < GRID_SIZE / 2; i++) {
       });
       cell.addTo(map);
 
-      const tokenValue = spawnToken(i, j);
-      if (tokenValue != null) {
-        addTokenLabel(i, j, tokenValue);
-      }
+      spawnToken(i, j);
     }
   }
 }
